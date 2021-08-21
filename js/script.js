79e8242a053dc8e55d1371ea8eb1eea39976696b
ruhul-7451 <csehastakenmylife@gmail.com>
2021-08-21 15:36:02 +0600

    finishing assingment

--- js/script.js
+++ js/script.js
@@ -4,15 +4,21 @@ function updateInvoiceField(customItem, price) {
     const updatePrice = parseInt(basePrice.innerText + price);
     basePrice.innerText = updatePrice;
 
-    /* Calculate Total */
+    /* Calculate Total by calling funtion */
     calculateTotal();
 };
 
+function getItemPrice(item) {
+    const itemPrice = parseInt(document.getElementById(item).innerText);
+    return itemPrice;
+}
+
 function calculateTotal() {
-    const basePrice = parseInt(document.getElementById('base-price').innerText) + parseInt(document.getElementById('extra-memory-cost').innerText) + parseInt(document.getElementById('extra-storage-cost').innerText) + parseInt(document.getElementById('delivery-charge').innerText);
-    // basePrice3 = basePrice + basePrice2;
-    document.getElementById('sub-total-cost').innerText = basePrice;
-    document.getElementById('total-cost').innerText = basePrice;
+    const itemsPriceTotal = getItemPrice('base-price') + getItemPrice('extra-memory-cost') + getItemPrice('extra-storage-cost') + getItemPrice('delivery-charge');
+
+    /* Updating invoice field total prices */
+    document.getElementById('sub-total-cost').innerText = itemsPriceTotal;
+    document.getElementById('total-cost').innerText = itemsPriceTotal;
 }
 
 
@@ -24,13 +30,13 @@ document.getElementById('memory-optn-16gb').addEventListener('click', function (
     updateInvoiceField('extra-memory-cost', 180);
 });
 document.getElementById('storage-optn-256gb').addEventListener('click', function () {
-    updateInvoiceField('extra-storage-cost', 280);
+    updateInvoiceField('extra-storage-cost', 0);
 });
 document.getElementById('storage-optn-512gb').addEventListener('click', function () {
-    updateInvoiceField('extra-storage-cost', 380);
+    updateInvoiceField('extra-storage-cost', 100);
 });
 document.getElementById('storage-optn-1tb').addEventListener('click', function () {
-    updateInvoiceField('extra-storage-cost', 500);
+    updateInvoiceField('extra-storage-cost', 180);
 });
 document.getElementById('free-delivery').addEventListener('click', function () {
     updateInvoiceField('delivery-charge', 0);
@@ -40,7 +46,11 @@ document.getElementById('paid-delivery').addEventListener('click', function () {
 });
 
 document.getElementById('apply-coupon').addEventListener('click', function () {
+    const couponCodeField = document.getElementById('coupon-code');
     const couponCode = document.getElementById('coupon-code').value;
+    couponCodeField.value = '';
+
+    /* Coupon validation for 20% DISCOUNT */
     if (couponCode == 'stevekaku') {
         const totalAmountText = document.getElementById('total-cost');
         const totalAmount = totalAmountText.innerText;
